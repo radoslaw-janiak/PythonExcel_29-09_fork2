9add61b0987af43fbc44b024775b21b8c676c885
radoslawjaniak <rajkokret660@gmail.com>
2025-10-06 22:21:30 +0200

    add makro.bas

---
+++ makro/run_macro.py
@@ -0,0 +1,16 @@
+import xlwings as xw, psutil
+# działa tylko na windows
+# Nowy plik Excela
+wb = xw.Book()
+
+# Import modułu VBA
+wb.api.VBProject.VBComponents.Import(r"C:\Users\radek\PycharmProjects\PythonExcelXlwings\makro.bas")
+
+# Zapisz jako plik z makrami (.xlsm)
+wb.save(r"C:\Users\radek\PycharmProjects\PythonExcelXlwings\plik_z_makrem.xlsm")
+
+# Uruchom makro
+makro = wb.macro("HelloWorld")
+makro()
+
+wb.close()
